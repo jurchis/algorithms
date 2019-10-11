545bd315595ebcb68ae31ed58b0ed1f348e04422
Florin Jurchis <jurchis@gmail.com>
2019-10-11 16:50:41 +0300

    solved CozaLozaWoza final

--- src/main/java/com/milnesium/CozaWozaLoza.java
+++ src/main/java/com/milnesium/CozaWozaLoza.java
@@ -1,22 +1,40 @@
 package com.milnesium;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+import static com.milnesium.CozaLozaWoza.printCozaLozaWoza;
+
 class CozaLozaWoza {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 110; i++) {
+        printCozaLozaWoza();
+    }
+
+    public static void printCozaLozaWoza() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the start number:");
+        int start = scanner.nextInt();
+        System.out.println("Please enter the end number:");
+        int end = scanner.nextInt();
+        for (int i = start; i <= end; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.print("CozaLoza" + " ");
+                System.out.print("CozaLoza");
             } else if (i % 3 == 0) {
-                System.out.print("Coza" + " ");
+                System.out.print("Coza");
             } else if (i % 5 == 0) {
-                System.out.print("Loza" + " ");
+                System.out.print("Loza");
             } else if (i % 7 == 0) {
-                System.out.print("Woza" + " ");
+                System.out.print("Woza");
             } else {
-                System.out.print(i + " ");
+                System.out.print(i);
             }
 
-            if (i % 11 == 0){
+            if (i % 11 != 0) {
+                System.out.print(" ");
+            }
+            if (i % 11 == 0) {
                 System.out.println();
             }
         }
